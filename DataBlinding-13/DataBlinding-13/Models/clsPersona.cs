11f9a15aa5d7ae6f6f2d8358bcbe552d5cbee4ab
Adrian Pol <adripol1994@gmail.com>
2016-11-08 13:54:57 +0100

    DataBlinding-13

---
+++ DataBlinding-13/DataBlinding-13/Models/clsPersona.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBlinding_13.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class clsPersona
+    {
+        //Atributos
+        public int ID { get; set; }
+        [Required]
+        public String Nombre { get; set; }
+        [Required]
+        public String Apellidos { get; set; }
+        [DisplayFormat(DataFormatString ="{0:d}", ApplyFormatInEditMode =true)]
+        public DateTime FechaNac { get; set; }
+        
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+
+        //Constructor por defecto
+        public clsPersona()
+        {
+            this.ID = 0;
+            this.Nombre = "";
+            this.Apellidos = "";
+            this.FechaNac = new DateTime();
+            this.Direccion = "";
+            this.Telefono = "";
+        }
+
+        //Constructor por parámetros
+        public clsPersona (int id, String nombre, String apellidos, DateTime fechaNac, String direccion, String telefono)
+        {
+            this.ID = id;
+            this.Nombre = nombre;
+            this.Apellidos = apellidos;
+            this.FechaNac = fechaNac;
+            this.Direccion = direccion;
+            this.Telefono = telefono;
+        }
+    }
+}
---
+++ DataBlinding-13/DataBlinding-13/ViewModels/clsMainPageVM.cs
@@ -0,0 +1,46 @@
+﻿using DataBlinding_13.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBlinding_13.ViewModels
+{
+    class MainPageVM
+    {
+        private clsPersona _personaSeleccionada;
+        private ObservableCollection<clsPersona> _listado;
+
+        public MainPageVM()
+        {
+
+            _listado = new ListadoPersona().listaPerona();
+        }
+
+        public ObservableCollection<clsPersona> listado
+        {
+            get
+            {
+                return _listado;
+            }
+        }
+
+
+
+        public clsPersona personaSeleccionada
+        {
+            get
+            {
+                return _personaSeleccionada;
+            }
+            set
+            {
+                _personaSeleccionada = value;
+            }
+        }
+
+
+    }
+}

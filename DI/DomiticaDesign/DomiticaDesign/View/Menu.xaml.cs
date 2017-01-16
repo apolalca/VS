6de0ac7e45e70c8ee9f5681731e51105dd70da56
adripol94 <adripol94@ADRINPOLALCD7A8>
2017-01-17 00:58:47 +0100

    Principal casi terminar

--- DI/DomiticaDesign/DomiticaDesign/View/Menu.xaml.cs
+++ DI/DomiticaDesign/DomiticaDesign/View/Menu.xaml.cs
@@ -30,8 +30,39 @@ namespace DomiticaDesign.View
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
             MySplitView.IsPaneOpen = true;
+            MenuButton1.Visibility = Visibility.Visible;
+        }
 
-            
+        private void PaginaPrincipal_Click(object sender, RoutedEventArgs e)
+        {
+            Row1.Height = new GridLength(1 ,GridUnitType.Star);
+            Row2.Height = new GridLength(0);
+            Row3.Height = new GridLength(0);
+            Row4.Height = new GridLength(0);
+        }
+
+        private void Luz_Click(object sender, RoutedEventArgs e)
+        {
+            Row1.Height = new GridLength(0);
+            Row2.Height = new GridLength(1, GridUnitType.Star);
+            Row3.Height = new GridLength(0);
+            Row4.Height = new GridLength(0);
+        }
+
+        private void Persianas_Click(object sender, RoutedEventArgs e)
+        {
+            Row1.Height = new GridLength(0);
+            Row2.Height = new GridLength(0);
+            Row3.Height = new GridLength(1, GridUnitType.Star);
+            Row4.Height = new GridLength(0);
+        }
+
+        private void Alarma_Click(object sender, RoutedEventArgs e)
+        {
+            Row1.Height = new GridLength(0);
+            Row2.Height = new GridLength(0);
+            Row3.Height = new GridLength(0);
+            Row4.Height = new GridLength(1, GridUnitType.Star);
         }
     }
 }

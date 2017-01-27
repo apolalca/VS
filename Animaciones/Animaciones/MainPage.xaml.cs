f01a3fa075e85d2951f4fc2d56513fe0f0ff5594
Adrian Pol <adripol1994@gmail.com>
2017-01-27 14:28:29 +0100

    Animations Init

---
+++ Animaciones/Animaciones/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Animaciones.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Animaciones
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public ObservableCollection<Person> persons
+        {
+            get
+            {
+                ObservableCollection<Person> p = new ObservableCollection<Person>();
+                p.Add(new Person("Adrian", "Pol"));
+                p.Add(new Person("Adrian", "Pol"));
+                p.Add(new Person("Adrian", "Pol"));
+                p.Add(new Person("Adrian", "Pol"));
+
+                return p;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Page2));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            gvPersons.ItemsSource = persons;
+        }
+    }
+}
---
+++ Animaciones/Animaciones/Models/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animaciones.Models
+{
+    public class Person
+    {
+        private String nombre;
+        private String apellido;
+
+        public Person(String nombre, String apellido)
+        {
+
+        }
+
+        public String Nombre
+        {
+            set
+            {
+                this.nombre = value;
+            }
+
+            get
+            {
+                return this.nombre;
+            }
+        }
+
+        public String Apellido
+        {
+            set
+            {
+                this.apellido = value;
+            }
+            get
+            {
+                return this.apellido;
+            }
+        }
+    }
+}
